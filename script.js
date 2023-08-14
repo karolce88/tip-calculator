851a4e47f89f0bd5afd2b996a10083c58633316b
Karol <karolcwiklik@o2.pl>
2023-08-14 19:03:16 +0200

    done

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const search = document.querySelector('.search')
-const li = document.querySelectorAll('li')
+const price = document.querySelector('#price')
+const people = document.querySelector('#people')
+const tip = document.querySelector('#tip')
+const error = document.querySelector('.error')
+const countBtn = document.querySelector('.count')
+const costInfo = document.querySelector('.cost-info')
+const cost = document.querySelector('.cost')
 
-const searchEngine = (e) => {
-    const textFromInput = e.target.value.toLowerCase()
+const showBill = () => {
 
-    li.forEach(textElement =>{
-
-        if (textElement.textContent.toLocaleLowerCase().indexOf(textFromInput) !== -1) { 
-            textElement.style.display = 'flex'
-        } else { 
-            textElement.style.display = 'none'
-        }
-    })
+    if (price.value == '' || tip.value == 0 || people.value == 0) {
+        error.textContent = 'Uzupełnij wszystkie pola!';
+        costInfo.style.display = 'none';
+    }
+    else {
+        error.textContent = '';
+        countBill();
+    }
 }
 
-search.addEventListener('keyup', searchEngine)
+const countBill = () => {
+    const newPrice = parseFloat(price.value);
+    const newPeople = parseFloat(people.value);
+    const newTip = parseFloat(tip.value);
+
+    const sum = ((newPrice * newTip) + newPrice) / newPeople;
+    costInfo.style.display = 'block';
+
+    cost.textContent = sum.toFixed(2)
+    console.log(sum);
+}
+
+countBtn.addEventListener('click', showBill)
